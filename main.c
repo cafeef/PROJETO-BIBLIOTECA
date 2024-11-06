9ec41c40c525f853c2360dd43bede801d31b8a50
Fernanda Pacheco <cafeef16@gmail.com>
2024-11-06 09:38:04 -0300

    iniciei o arquivo livro e armazenei nas structs

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ void limpar();
 
 typedef struct livros {
     int codigo;
-    char titulo[20], autor[15], genero[15];
+    char titulo[20], autor[20], genero[15];
     int status; //1 - disponivel | 2 - reservado | 3 - emprestado
     int num_reservas, qnt_total;
 } Tlivro;
@@ -36,8 +36,26 @@ typedef struct reserva {
 } Treserva;
 
 int main() {
-   limpar();
-   SetConsoleOutputCP(CP_UTF8);
+    limpar();
+    SetConsoleOutputCP(CP_UTF8);
+    FILE *livros, *leitores, *funcionarios, *emprestimos, *reservas; //criação dos ponteiros que irão ler os arquivos
+    Tlivro *plivros; //criação do ponteiro que vai mexer com a struct
+    int i, num_linhas; //contador i pro for e num_linhas pra quantidade de linhas de cada arquivo
+    livros = fopen("C:.\\dados\\livros.txt", "r"); //abrindo um arquivo pelo windows
+    if(!livros) { //verificação se o arquivo foi aberto
+        printf("Arquivo não aberto\n");
+        exit(1);
+    }
+    printf("Sucesso na abertura do arquivo\n"); 
+    fscanf(livros, "%d", &num_linhas); //recebimento da quantidade de linhas do arquivo livros
+    if (num_linhas == 5) //verificação
+        printf("Sucesso na leitura de tamanho\n");
+    plivros = (Tlivro *)malloc(sizeof(Tlivro) * num_linhas); //criação de um vetor de structs dinâmico
+    for (i = 0; i < num_linhas; i++) { //leitura dos dados e armazenamento nas structs
+        fscanf(livros, "%d %s %s %s %d %d %d", &(plivros + i)->codigo, &(plivros + i)->titulo, &(plivros + i)->autor, &(plivros + i)->genero, &(plivros + i)->status, &(plivros + i)->num_reservas, &(plivros + i)->qnt_total);
+    }
+    for (i = 0; i < num_linhas; i++) //impressão de cada struct
+        printf("%d %s %s %s %d %d %d\n", ((plivros + i)->codigo), ((plivros + i)->titulo), ((plivros + i)->autor), ((plivros + i)->genero), ((plivros + i)->status), ((plivros + i)->num_reservas), ((plivros + i)->qnt_total));
 
    printf("| BIBLIOTECA VIRTUAL\n\n1 | Novo acesso\n2 | Consulta de acervo\n3 | Relatórios\n4 | Administração");
 
